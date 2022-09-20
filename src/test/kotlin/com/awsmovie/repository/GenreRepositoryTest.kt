c480f7f5df5390dd853be3dfbc13c48f00985d30
junseo <djawnstj44@naver.com>
2022-09-20 22:22:39 +0900

    Test: GenreRepositoryTest 생성
    GenreRepositoryTest 생성
    장르 저장 테스트 코드 추가

---
+++ src/test/kotlin/com/awsmovie/repository/GenreRepositoryTest.kt
@@ -0,0 +1,32 @@
+package com.awsmovie.repository
+
+import com.awsmovie.entity.genre.Genre
+import com.awsmovie.repository.genre.GenreRepository
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import javax.transaction.Transactional
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest
+@Transactional
+class GenreRepositoryTest @Autowired constructor(
+    private val genreRepository: GenreRepository
+) {
+
+    @Test
+    fun 장르_저장() {
+        // given
+        val genre = Genre.createGenre(0)
+
+        // when
+        val savedGenre = genreRepository.save(genre)
+
+        // then
+        assertEquals(genre.genreId, savedGenre)
+    }
+
+}

a6582b92836bed2f4c902fe82670fb3658addb67
junseo <djawnstj44@naver.com>
2022-10-10 20:59:52 +0900

    Test: Genre where in 절 조회 test 추가
    Genre where in 절 조회 test 추가

--- src/test/kotlin/com/awsmovie/repository/GenreRepositoryTest.kt
+++ src/test/kotlin/com/awsmovie/repository/GenreRepositoryTest.kt
@@ -1,7 +1,7 @@
 package com.awsmovie.repository
 
 import com.awsmovie.entity.genre.Genre
-import com.awsmovie.entity.genre.GenreEnum
+import com.awsmovie.entity.genre.GenreCode
 import com.awsmovie.repository.genre.GenreRepository
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
@@ -21,13 +21,32 @@ class GenreRepositoryTest @Autowired constructor(
     @Test
     fun 장르_저장() {
         // given
-        val genre = Genre.createGenre(GenreEnum.ROMANCE)
+        val genre = Genre.createGenre(GenreCode.ROMANCE)
 
         // when
         val savedGenre = genreRepository.save(genre)
 
         // then
         assertEquals(genre.genreId, savedGenre)
+    }
+
+    @Test
+    fun 장르_조회() {
+        // given
+        val genre1 = Genre.createGenre(GenreCode.ROMANCE)
+        val genre2 = Genre.createGenre(GenreCode.ACTION)
+
+        genreRepository.save(genre1)
+        genreRepository.save(genre2)
+
+        val genreList = listOf(genre1.genre, genre2.genre)
+
+        // when
+        val genres = genreRepository.findByGenreIn(genreList)
+
+        // then
+        genres.forEach { println(it.genreId) }
+
     }
 
 }

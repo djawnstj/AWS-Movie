108914882221a78395b637d2b2904c44a11dbe13
junseo <djawnstj44@naver.com>
2022-10-10 20:59:34 +0900

    Feat: Genre where in 절 조회 추가
    Genre where in 절 조회 추가

--- src/main/kotlin/com/awsmovie/repository/genre/GenreRepository.kt
+++ src/main/kotlin/com/awsmovie/repository/genre/GenreRepository.kt
@@ -1,9 +1,10 @@
 package com.awsmovie.repository.genre
 
 import com.awsmovie.entity.genre.Genre
+import com.awsmovie.entity.genre.GenreCode
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
-import java.util.*
 
 @Repository
 interface GenreRepository: JpaRepository<Genre, Long> {
@@ -12,5 +13,10 @@ interface GenreRepository: JpaRepository<Genre, Long> {
      * 장르 저장
      */
     fun save(genre: Genre): Genre
+
+    /**
+     * 장르 조회
+     */
+    fun findByGenreIn(@Param("genre") genres: List<GenreCode>): List<Genre>
 
 }

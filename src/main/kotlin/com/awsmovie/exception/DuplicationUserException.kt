c4e60870acc8fdbd8954945eba3bddeea8a7eef8
junseo <djawnstj44@naver.com>
2022-10-16 17:22:43 +0900

    Comment: 회원 중복 체크 예외 클래스 설명 주석 추가
    회원 중복 체크 예외 클래스 설명 주석 추가

--- src/main/kotlin/com/awsmovie/exception/DuplicationUserException.kt
+++ src/main/kotlin/com/awsmovie/exception/DuplicationUserException.kt
@@ -1,3 +1,6 @@
 package com.awsmovie.exception
 
+/**
+ * 중복 회원 예외 클래스
+ */
 class DuplicationUserException(message: String): IllegalStateException(message)
---
+++ src/main/kotlin/com/awsmovie/exception/ValidationUserException.kt
@@ -0,0 +1,4 @@
+package com.awsmovie.exception
+
+class ValidationUserException {
+}

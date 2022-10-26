29a347083fc3c6981b85e368afb2e5886635a1ae
JUNSEO <djawnstj44@naver.com>
2022-10-26 15:32:58 +0900

    Feat: GenreCodeReqConverter 키값 변경
    GenreCodeReqConverter 키값 변경
    Int -> String

--- src/main/kotlin/com/awsmovie/controller/converter/GenreCodeReqConverter.kt
+++ src/main/kotlin/com/awsmovie/controller/converter/GenreCodeReqConverter.kt
@@ -3,8 +3,8 @@ package com.awsmovie.controller.converter
 import com.awsmovie.entity.movie.genre.GenreCode
 import org.springframework.core.convert.converter.Converter
 
-class GenreCodeReqConverter: Converter<Int, GenreCode> {
+class GenreCodeReqConverter: Converter<String, GenreCode> {
 
-    override fun convert(genreCode: Int): GenreCode = GenreCode.of(genreCode)
+    override fun convert(genreCode: String): GenreCode = GenreCode.of(genreCode.toInt())
 
 }

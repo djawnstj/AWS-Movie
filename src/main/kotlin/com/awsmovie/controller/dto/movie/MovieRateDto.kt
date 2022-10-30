1d814010a3808df9b098dd494f1b0c2de2b2a68a
junseo <djawnstj44@naver.com>
2022-10-30 15:45:31 +0900

    Feat: MovieRateDto 필드 추가
    MovieRateDto movieId 필드 추가

--- src/main/kotlin/com/awsmovie/controller/dto/movie/MovieRateDto.kt
+++ src/main/kotlin/com/awsmovie/controller/dto/movie/MovieRateDto.kt
@@ -3,7 +3,8 @@ package com.awsmovie.controller.dto.movie
 import com.awsmovie.controller.dto.user.UserDto
 
 data class MovieRateDto(
-    val user: UserDto,
+    val user: UserDto?,
+    val movieId: Long,
     val rate: Int,
     val comment: String
 )

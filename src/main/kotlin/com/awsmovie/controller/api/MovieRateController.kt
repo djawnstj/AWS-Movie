191042444b19f826f6e2fc1c42a649f682de2a65
junseo <djawnstj44@naver.com>
2022-10-30 15:46:20 +0900

    Feat: RedisSession 어노테이션 추가, 평점 생성 api RequestBody 로 변경
    RedisSession 어노테이션 추가, 평점 생성 api RequestBody 로 변경

--- src/main/kotlin/com/awsmovie/controller/api/MovieRateController.kt
+++ src/main/kotlin/com/awsmovie/controller/api/MovieRateController.kt
@@ -5,36 +5,42 @@ import com.awsmovie.controller.dto.user.UserDto
 import com.awsmovie.controller.response.BaseResponse
 import com.awsmovie.controller.response.ListResponse
 import com.awsmovie.service.movie.MovieRateService
+import com.awsmovie.util.StringUtil.isNumeric
 import lombok.RequiredArgsConstructor
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
 import org.springframework.web.bind.annotation.*
+import javax.servlet.http.HttpSession
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/aws-movie-api/v1")
+@EnableRedisHttpSession
 class MovieRateController(
     private val movieRateService: MovieRateService
 ) {
 
     @PostMapping("/movie-rates")
     fun create(
-        @RequestParam uid: Long,
-        @RequestParam movieId: Long,
-        @RequestParam rate: Int,
-        @RequestParam comment: String
+        @RequestBody movieRateDto: MovieRateDto,
+        session: HttpSession
     ): BaseResponse {
 
-        val movieRate = movieRateService.saveRate(uid, movieId, rate, comment)
+        val uid = session.getAttribute("uid")
+
+//        check(uid.isNumeric()) { throw IllegalStateException("유효한 세션값이 아닙니다.") }
+
+        val movieRate = movieRateService.saveRate(uid.toString().toLong(), movieRateDto.movieId, movieRateDto.rate, movieRateDto.comment)
 
         movieRate.apply {
 
             val userDto = UserDto(user.userName, user.userId,)
 
-            val movieRateDto = MovieRateDto(userDto, rate, comment)
+//            val movieRateDto = MovieRateDto(userDto, rate, comment)
 
             return ListResponse(
                 HttpStatus.OK.value(),

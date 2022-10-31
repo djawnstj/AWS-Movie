1503056bcf404a1700059004313352a5c408a77d
JUNSEO <djawnstj44@naver.com>
2022-10-31 14:50:40 +0900

    Feat: UserDto uid 필드 추가
    UserDto uid 필드 추가

--- src/main/kotlin/com/awsmovie/controller/api/MovieRateController.kt
+++ src/main/kotlin/com/awsmovie/controller/api/MovieRateController.kt
@@ -1,17 +1,14 @@
 package com.awsmovie.controller.api
 
 import com.awsmovie.controller.dto.movie.MovieRateDto
-import com.awsmovie.controller.dto.user.UserDto
 import com.awsmovie.controller.response.BaseResponse
 import com.awsmovie.controller.response.ListResponse
 import com.awsmovie.service.movie.MovieRateService
-import com.awsmovie.util.StringUtil.isNumeric
 import lombok.RequiredArgsConstructor
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
 import org.springframework.web.bind.annotation.*
 import javax.servlet.http.HttpSession
@@ -30,17 +27,9 @@ class MovieRateController(
         session: HttpSession
     ): BaseResponse {
 
-        val uid = session.getAttribute("uid")
-
-//        check(uid.isNumeric()) { throw IllegalStateException("유효한 세션값이 아닙니다.") }
-
-        val movieRate = movieRateService.saveRate(uid.toString().toLong(), movieRateDto.movieId, movieRateDto.rate, movieRateDto.comment)
+        val movieRate = movieRateService.saveRate(movieRateDto.user?.uid.toString().toLong(), movieRateDto.movieId, movieRateDto.rate, movieRateDto.comment)
 
         movieRate.apply {
-
-            val userDto = UserDto(user.userName, user.userId,)
-
-//            val movieRateDto = MovieRateDto(userDto, rate, comment)
 
             return ListResponse(
                 HttpStatus.OK.value(),
